afbe9deeac53bcb40a0e948f16903a73d7bc0af6
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-10-28 08:23:31 -0600

    Update package list in 'build.sh'

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,7 +5,11 @@ set -ouex pipefail
 mkdir -p /nix
 
 # install some extra tools
-dnf5 install --enable-repo=terra -y qt5-qttools ghostty tmux
+dnf5 install --enable-repo=terra -y \
+  qt5-qttools \
+  qt6-qttools \
+  tmux \
+  kitty kitty-shell-integration kitty-terminfo
 
 # clean up after ourselves
 dnf5 clean all &&
