22320c2c0751f58cf26bd669c54545458c78c67b
WombatFromHell <WombatFromHell@users.noreply.github.com>
2025-11-27 15:02:31 -0700

    Add 'cliphist' and 'fuzzel' to packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,12 +13,12 @@ dnf5 install --enable-repo=terra -y \
   kitty kitty-shell-integration kitty-terminfo \
   ghostty ghostty-bat-syntax ghostty-shell-integration ghostty-terminfo
 
-# include 'niri', 'dms', and 'quickshell' from a verified repo
+# include 'niri', 'dms', 'fuzzel', and 'quickshell' from a verified repo
 dnf5 -y copr enable avengemedia/dms &&
-  dnf5 -y install quickshell niri dms
-# include 'noctalia-shell' from third-party (unverified) repo
+  dnf5 -y install quickshell niri dms fuzzel
+# include 'noctalia-shell' and 'cliphist' from third-party (unverified) repo
 dnf5 -y copr enable zhangyi6324/noctalia-shell &&
-  dnf5 -y install noctalia-shell
+  dnf5 -y install noctalia-shell cliphist
 
 # include faugus-launcher
 dnf5 -y copr enable faugus/faugus-launcher &&
