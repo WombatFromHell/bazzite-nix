60ec1c6f0d33b8b8ef5f7db1561f508b8087709e
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-02-16 08:27:06 -0700

    Re-enable COPR cachyos kernel in build.sh

--- Containerfile
+++ Containerfile
@@ -1,5 +1,6 @@
 # Base Image (default to 'stable')
 ARG BASE_IMAGE=ghcr.io/ublue-os/bazzite:stable
+ARG BUILD_SCRIPT=build.sh
 
 # Allow build scripts to be referenced without being copied into the final image
 FROM scratch AS ctx
@@ -11,7 +12,7 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
   --mount=type=cache,dst=/var/cache \
   --mount=type=cache,dst=/var/log \
   --mount=type=tmpfs,dst=/tmp \
-  /ctx/build.sh && \
+  /ctx/"${BUILD_SCRIPT}" && \
   ostree container commit
 
 ### LINTING
--- build_files/build-cachyos.sh
+++ build_files/build-cachyos.sh
@@ -4,6 +4,25 @@ set -ouex pipefail
 # nix installer enablement
 mkdir -p /nix
 
+# use cachyos kernel for fedora instead of bazzite kernel
+export TMPDIR=/var/tmp
+mkdir -p $TMPDIR
+setsebool -P domain_kernel_load_modules on
+
+dnf5 -y copr enable bieszczaders/kernel-cachyos &&
+  dnf5 -y remove \
+    kernel kernel-core \
+    kernel-devel-matched kernel-devel \
+    kernel-modules \
+    kernel-modules-akmods \
+    kernel-modules-core \
+    kernel-modules-extra &&
+  dnf5 -y install --setopt=tsflags=noscripts kernel-cachyos
+
+KERNEL_VERSION=$(rpm -qa --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}' kernel-cachyos-core)
+depmod -a "${KERNEL_VERSION}" &&
+  kernel-install add "${KERNEL_VERSION}" "/lib/modules/${KERNEL_VERSION}/vmlinuz"
+
 # install some extra tools
 dnf5 -y install --enable-repo=terra \
   qt5-qttools qt6-qttools \
