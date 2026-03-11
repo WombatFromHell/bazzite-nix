93a5f43b82dfef5be40aeb78613eb9a427ab6583
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-03-11 17:48:10 -0600

    Include additional deps for an improved Niri experience

--- build_files/extras.sh
+++ build_files/extras.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# include niri and friends from a verified repo
+# include niri + DMS and friends from a verified repo
 dnf5 -y copr enable avengemedia/dms-git &&
-  dnf5 -y install niri dms danksearch dgop fuzzel kanshi
+  dnf5 -y install niri dms danksearch dgop fuzzel kanshi cava matugen cups-pk-helper xdg-desktop-portal-kde
