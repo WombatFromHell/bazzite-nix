d3df4a414a1208aeb2b7f3d79c6e62f64b5374bc
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-03-11 15:55:20 -0600

    Update niri related deps in 'extras.sh'

--- build_files/extras.sh
+++ build_files/extras.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# include 'niri', 'dms', 'fuzzel', 'kanshi', and 'quickshell' from a verified repo
-dnf5 -y copr enable avengemedia/dms &&
-	dnf5 -y install quickshell niri dms fuzzel kanshi
-# include 'noctalia-shell' and 'cliphist' from third-party (unverified) repo
-dnf5 -y copr enable zhangyi6324/noctalia-shell &&
-	dnf5 -y install noctalia-shell cliphist
+# include niri and friends from a verified repo
+dnf5 -y copr enable avengemedia/dms-git &&
+  dnf5 -y install niri dms danksearch dgop fuzzel kanshi
