5207928bf1d8d4ff36b9b81308820f5b62cea1ba
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-03-13 10:41:05 -0600

    Include a niri-portals.conf override customized for use with KDE

--- build_files/extras.sh
+++ build_files/extras.sh
@@ -3,3 +3,12 @@
 # include niri + DMS and friends from a verified repo
 dnf5 -y copr enable avengemedia/dms-git &&
   dnf5 -y install niri dms danksearch dgop fuzzel kanshi cava matugen cups-pk-helper xdg-desktop-portal-kde
+
+# use our niri-portals.conf override customized for KDE
+PORTALS_GLOBAL="/usr/share/xdg-desktop-portal"
+PORTALS_OVERRIDE="/ctx/override/usr/share/xdg-desktop-portal"
+if [ -r "$PORTALS_GLOBAL"/niri-portals.conf ]; then
+  # backup our default niri-portals.conf
+  mv "$PORTALS_GLOBAL"/niri-portals.conf "$PORTALS_GLOBAL"/niri-portals.conf.bak
+  cp -f "$PORTALS_OVERRIDE"/niri-portals.conf "$PORTALS_GLOBAL"/niri-portals.conf
+fi
