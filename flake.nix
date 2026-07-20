c481da739202df10c53cdf3df542d1b4af446e87
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-07-20 14:41:46 -0600

    Switch to detsys' chilled nixpkgs in our flake.nix devshell

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Development environment with Python and pytest";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-26.05-chilled/0.1";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
