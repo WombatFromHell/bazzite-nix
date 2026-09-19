adf82d443d0a8dcd53d6599250437e07b91b6588
WombatFromHell <WombatFromHell@users.noreply.github.com>
2026-09-19 09:49:50 -0600

    Replace Terra 'xwayland-satellite' with our niri-spicy-builder output

--- build_files/extras.sh
+++ build_files/extras.sh
@@ -32,7 +32,7 @@ dnf5 config-manager setopt terra.baseurl="https://repos.fyralabs.com/terra\$rele
 dnf5_retry -y install --refresh --enable-repo=terra \
   rocm-smi uwsm qt5-qttools qt6-qttools \
   tmux gvfs-smb gvfs-fuse openrgb openrgb-udev-rules \
-  gamescope-session-steam xwayland-satellite
+  gamescope-session-steam
 # shellcheck disable=SC2140
 dnf5 config-manager setopt "*terra*".exclude="nerd-fonts scx-tools scx-scheds python3-protobuf zlib-devel uupd"
 
@@ -63,8 +63,9 @@ install -Z -D -m 0644 \
   "$OVERRIDES_ROOT"/usr/share/uwsm/env-niri \
   /usr/share/uwsm/env-hyprland
 
-# use our pre-built niri-spicy RPM
-dnf5_retry install -y https://github.com/WombatFromHell/niri-spicy-builder/releases/download/v26.04.git%2B15c93f6-1.fc44/niri-26.04.git+15c93f6-1.fc44.x86_64.rpm
+# use our pre-built niri-spicy RPM's
+dnf5_retry install -y https://github.com/WombatFromHell/niri-spicy-builder/releases/download/v26.04.git%2B641335f-1.fc44/xwayland-satellite-0.8.2.git+add2795-1.fc44.x86_64.rpm
+dnf5_retry install -y https://github.com/WombatFromHell/niri-spicy-builder/releases/download/v26.04.git%2B641335f-1.fc44/niri-26.04.git+641335f-1.fc44.x86_64.rpm
 
 # include DMS and friends from a verified repo
 dnf5 -y copr enable avengemedia/dms-git &&
